e8f7a8371f7a7bd9c9728b63ffef19f74e185891
SnakeGuitar <snakersito08@gmail.com>
2026-04-02 15:13:56 -0600

    feat: add theme combobox in settings view

--- MemoryGame.Client/ViewModels/Settings/SettingsViewModel.cs
+++ MemoryGame.Client/ViewModels/Settings/SettingsViewModel.cs
@@ -16,6 +16,7 @@ public partial class SettingsViewModel : ObservableObject
     private readonly ClientSettings     _settings;
     private readonly MusicService       _music;
     private readonly IWindowService     _window;
+    private readonly ThemeService       _theme;
 
     // Always shown in native script so the user can find their language regardless of current UI language.
     public static IReadOnlyList<LanguageOption> Languages { get; } =
@@ -32,23 +33,29 @@ public partial class SettingsViewModel : ObservableObject
     [ObservableProperty] private double _musicVolume;
     [ObservableProperty] private int    _selectedTrackIndex;
     [ObservableProperty] private bool   _isFullscreen;
+    [ObservableProperty] private string _selectedTheme;
+
+    public static IReadOnlyList<string> Themes => ThemeService.Themes;
 
     public SettingsViewModel(
         INavigationService navigation,
         ClientSettings     settings,
         MusicService       music,
-        IWindowService     window)
+        IWindowService     window,
+        ThemeService       theme)
     {
         _navigation = navigation;
         _settings   = settings;
         _music      = music;
         _window     = window;
+        _theme      = theme;
 
-        _selectedLanguage = Languages.FirstOrDefault(l => l.Code == settings.LanguageCode) ?? Languages[0];
-        _isMusicEnabled   = settings.MusicEnabled;
-        _musicVolume      = settings.MusicVolume;
+        _selectedLanguage   = Languages.FirstOrDefault(l => l.Code == settings.LanguageCode) ?? Languages[0];
+        _isMusicEnabled     = settings.MusicEnabled;
+        _musicVolume        = settings.MusicVolume;
         _selectedTrackIndex = music.CurrentTrackIndex;
-        _isFullscreen     = window.IsFullscreen;
+        _isFullscreen       = window.IsFullscreen;
+        _selectedTheme      = settings.ThemeName;
     }
 
     /// <summary>Get the list of available music tracks.</summary>
@@ -62,13 +69,11 @@ public partial class SettingsViewModel : ObservableObject
         LocalizationManager.Instance.SetCulture(value.Code);
     }
 
-    partial void OnIsMusicEnabledChanged(bool value) => _music.IsEnabled = value;
-
-    partial void OnMusicVolumeChanged(double value) => _music.Volume = value;
-
-    partial void OnSelectedTrackIndexChanged(int value) => _music.CurrentTrackIndex = value;
-
-    partial void OnIsFullscreenChanged(bool value) => _window.SetFullscreen(value);
+    partial void OnIsMusicEnabledChanged(bool value)     => _music.IsEnabled = value;
+    partial void OnMusicVolumeChanged(double value)      => _music.Volume = value;
+    partial void OnSelectedTrackIndexChanged(int value)  => _music.CurrentTrackIndex = value;
+    partial void OnIsFullscreenChanged(bool value)       => _window.SetFullscreen(value);
+    partial void OnSelectedThemeChanged(string value)    => _theme.Apply(value);
 
     [RelayCommand]
     private void GoBack() => _navigation.GoBack();
