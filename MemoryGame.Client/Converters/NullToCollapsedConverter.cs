b46ce66a67af35dbcadd3636501d16294a619d63
SnakeGuitar <snakersito08@gmail.com>
2026-04-02 23:51:35 -0600

    feat: add converters

--- MemoryGame.Client/Converters/NullToCollapsedConverter.cs
+++ MemoryGame.Client/Converters/NullToCollapsedConverter.cs
@@ -10,8 +10,13 @@ namespace MemoryGame.Client.Converters;
 /// </summary>
 public class NullToCollapsedConverter : IValueConverter
 {
-    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
-        => string.IsNullOrEmpty(value?.ToString()) ? Visibility.Collapsed : Visibility.Visible;
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var hasValue = !string.IsNullOrEmpty(value?.ToString());
+        var invert = parameter is string s && s.Equals("invert", StringComparison.OrdinalIgnoreCase);
+        if (invert) hasValue = !hasValue;
+        return hasValue ? Visibility.Visible : Visibility.Collapsed;
+    }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         => throw new NotSupportedException();
