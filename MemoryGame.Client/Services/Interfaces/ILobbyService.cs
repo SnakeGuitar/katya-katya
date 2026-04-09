9c27b6122e789993e2a2860ee3ae3e0ad065023d
SnakeGuitar <snakersito08@gmail.com>
2026-04-09 15:41:59 -0600

    refactor: relocate client services interfaces

---
+++ MemoryGame.Client/Services/Interfaces/ILobbyService.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MemoryGame.Client.Models.Lobby;
+
+namespace MemoryGame.Client.Services.Interfaces;
+
+/// <summary>
+/// Handles all pre-game and lobby management SignalR events (joining, kicks, friends, listing).
+/// </summary>
+public interface ILobbyService
+{
+    // Hub events
+    event Action<string>? LobbyCreated;
+    event Action<List<LobbyPlayerDto>>? PlayerListUpdated;
+    event Action<string, bool>? PlayerJoined;
+    event Action<string>? PlayerLeft;
+    event Action? Kicked;
+    
+    event Action<List<LobbySummaryDto>>? PublicLobbiesUpdated;
+    event Action<string, string>? LobbyInviteReceived;
+    event Action<string, bool>? LobbyInviteSent;
+    
+    event Action<string>? ErrorReceived;
+
+    // Hub methods
+    Task CreateLobbyAsync(string gameCode, bool isPublic);
+    Task JoinLobbyAsync(string gameCode);
+    Task LeaveLobbyAsync();
+    
+    Task VoteToKickAsync(string targetUsername);
+    Task GetPublicLobbiesAsync();
+    Task InviteFriendAsync(int targetUserId);
+}
