dd3cbe2fc6763750806833b02bb3d0182a8234a1
SnakeGuitar <snakersito08@gmail.com>
2026-05-29 00:27:42 -0600

    fix: enable window drag and resize on custom-chrome window

--- KatyaKatya/KatyaKatya/Views/MainWindow.axaml.cs
+++ KatyaKatya/KatyaKatya/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 
 namespace KatyaKatya.Views;
@@ -9,6 +10,21 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        DragArea.PointerPressed += (_, e) =>
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+                BeginMoveDrag(e);
+        };
+
+        ResizeN.PointerPressed  += (_, e) => BeginResizeDrag(WindowEdge.North, e);
+        ResizeS.PointerPressed  += (_, e) => BeginResizeDrag(WindowEdge.South, e);
+        ResizeW.PointerPressed  += (_, e) => BeginResizeDrag(WindowEdge.West,  e);
+        ResizeE.PointerPressed  += (_, e) => BeginResizeDrag(WindowEdge.East,  e);
+        ResizeNW.PointerPressed += (_, e) => BeginResizeDrag(WindowEdge.NorthWest, e);
+        ResizeNE.PointerPressed += (_, e) => BeginResizeDrag(WindowEdge.NorthEast, e);
+        ResizeSW.PointerPressed += (_, e) => BeginResizeDrag(WindowEdge.SouthWest, e);
+        ResizeSE.PointerPressed += (_, e) => BeginResizeDrag(WindowEdge.SouthEast, e);
     }
 
     private void OnCloseClicked(object? sender, RoutedEventArgs e)
